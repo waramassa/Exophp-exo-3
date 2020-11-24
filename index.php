d34ee137153ea4f93fb271e5ce3f968a3fe0fb03
Wara Massa <waramassa70@gmail.com>
2020-11-24 22:24:01 +0100

    php exo3

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP exo 3</title>
+</head>
+<body>
+    <p><?php echo $km; ?>
+    <p><?php echo $km = 3; ?>
+    <p><?php echo $km = 125; ?>
+
+    </p>
+    
+</body>
+</html>
